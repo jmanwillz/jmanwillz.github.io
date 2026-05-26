8ef4be5f157be6df70289b30315aa8385eb17913
jmanwillz <jman89412@gmail.com>
2026-05-26 18:31:17 +0000

    Deploying to gh-pages from @ jmanwillz/jmanwillz.github.io@301fcf63d5e58995c689ddfa7aa49784382d1704 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,32 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "nav-cv",
           title: "cv",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{
+        },{id: "post-hello-world",
+        
+          title: "Hello World 👋🏼",
+        
+        description: "this is the description, wow",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/hello-world/";
+          
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
